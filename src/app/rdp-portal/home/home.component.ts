708115350d30d53f3ea376009d5bfc85d9972f11
dieptran43 <levandiep44@gmail.com>
2018-03-08 17:55:32 +0700

    Update design for Index page

--- src/app/rdp-portal/home/home.component.ts
+++ src/app/rdp-portal/home/home.component.ts
@@ -7,7 +7,36 @@ import { ModalDirective, TooltipModule } from 'ngx-bootstrap';
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-    constructor() { }
+    public listSlider: any[] = [];
+    
+    constructor() {
+        
+     }
 
-    ngOnInit() { }
+
+    ngOnInit() {
+
+        this.listSlider = [
+            {
+                "imageName": "Slider1.jpg",
+                "title" : "Red Dot Power",
+                "description": "No.1 independent electricity retailer in Singapore"
+            },
+            {
+                "imageName": "Slider2.jpg",
+                "title" : "eSAVE",
+                "description": "Assured savings on your bill every month"
+            },
+            {
+                "imageName": "Slider3.jpg",
+                "title" : "eFIX",
+                "description": ""
+            },
+            {
+                "imageName": "Slider4.jpg",
+                "title" : "eQ",
+                "description": ""
+            }
+        ];
+     }
 }
